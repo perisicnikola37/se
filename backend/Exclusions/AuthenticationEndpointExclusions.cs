ecc949d9f4c9e67ca96bee13c0d1793775cff3ad
Nikola <nikola.perisic@vegait.rs>
2024-01-18 14:56:44 +0100

    Create Exclusion directory and AuthenticationEndpointExclusions.cs file in it

--- backend/Exclusions/AuthenticationEndpointExclusions.cs
+++ backend/Exclusions/AuthenticationEndpointExclusions.cs
@@ -1,8 +1,9 @@
 public static class AuthenticationEndpointExclusions
 {
-    public static List<string> ExcludedEndpoints => new List<string>
-    {
-        "/api/Auth/Login",
-        "/api/Auth/Register"
-    };
+	public static List<string> ExcludedEndpoints => new List<string>
+	{
+		"/api/Auth/Login",
+		"/api/Auth/Register"
+	};
 }
+
