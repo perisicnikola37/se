be30fa9047fa64e4e6bfdd27c66ed95712b558f8
Nikola <nikola.perisic@vegait.rs>
2024-01-18 11:20:15 +0100

    Finish Blog model input validation

--- backend/Models/Blog.cs
+++ backend/Models/Blog.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Vega.Models;
 
 public class Blog
 {
-    public int Id { get; set; }
-    public string Description { get; set; }
-    public string Author { get; set; }
-    public string Text { get; set; }
-    public DateTime Created_at { get; set; } = DateTime.Now;
+	public int Id { get; set; }
+	[Required(ErrorMessage = "Description is required")]
+	[StringLength(255, MinimumLength = 8, ErrorMessage = "Description must be between 8 and 255 characters")]
+	public string Description { get; set; }
+	public string Author { get; set; }
+	[Required(ErrorMessage = "Text is required")]
+	[StringLength(255, MinimumLength = 2, ErrorMessage = "Text must be between 2 and 1255 characters")]
+	public string Text { get; set; }
+	public DateTime Created_at { get; set; } = DateTime.Now;
 }
