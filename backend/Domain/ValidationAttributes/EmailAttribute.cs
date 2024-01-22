a582a7ce489a28c6015935ea5007e979bdda2c54
Nikola <nikola.perisic@vegait.rs>
2024-01-22 09:31:44 +0100

    remove warning: Possible null reference argument for parameter 'email' in 'Domain.ValidationAttributes.EmailValidationAttribute.IsValidEmail'

--- backend/Domain/ValidationAttributes/EmailAttribute.cs
+++ backend/Domain/ValidationAttributes/EmailAttribute.cs
@@ -2,25 +2,24 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace Domain.ValidationAttributes;
-    public class EmailValidationAttribute : ValidationAttribute
+
+public class EmailValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        public EmailValidationAttribute()
-        {
-        }
+        if (value is null || !IsValidEmail(value.ToString()))
+            return new ValidationResult(ErrorMessage ??
+                                        $"The field {validationContext.DisplayName} must be a valid email address.");
 
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value is null || !IsValidEmail(value.ToString()))
-            {
-                return new ValidationResult(ErrorMessage ?? $"The field {validationContext.DisplayName} must be a valid email address.");
-            }
-
-            return ValidationResult.Success;
-        }
-
-        private bool IsValidEmail(string email)
-        {
-            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
-            return Regex.IsMatch(email, pattern);
-        }
+        return ValidationResult.Success;
     }
+
+    private static bool IsValidEmail(string? email)
+    {
+        if (email is null)
+            return false;
+
+        const string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+        return Regex.IsMatch(email, pattern);
+    }
+}
