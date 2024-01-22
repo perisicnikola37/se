b9a200c47ed0268734755996bd0fa7e01bb6b3f8
Nikola <nikola.perisic@vegait.rs>
2024-01-22 10:47:54 +0100

    Throw ArgumentNullException if DefaultConnection is null

--- backend/ExpenseTrackerApi/Program.cs
+++ backend/ExpenseTrackerApi/Program.cs
@@ -23,7 +23,9 @@ builder.Services.AddCors(options =>
 
 // add DB context
 // add exception
-var connectionString = configuration?["DefaultConnection"] ?? "DefaultConnection";
+var connectionString = configuration["DefaultConnection"];
+if (connectionString == null) throw new ArgumentNullException(nameof(connectionString), "DefaultConnection is null");
+
 
 builder.Services.AddDbContext<MainDatabaseContext>(options =>
 {
@@ -52,9 +54,9 @@ builder.Services.AddAuthentication(options =>
     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(o =>
 {
-    var validIssuer = configuration?["Jwt:Issuer"] ?? "https://joydipkanjilal.com/";
-    var validAudience = configuration?["Jwt:Audience"] ?? "https://joydipkanjilal.com/";
-    var issuerSigningKey = configuration?["Jwt:Key"] ??
+    var validIssuer = configuration["Jwt:Issuer"] ?? "https://joydipkanjilal.com/";
+    var validAudience = configuration["Jwt:Audience"] ?? "https://joydipkanjilal.com/";
+    var issuerSigningKey = configuration["Jwt:Key"] ??
                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZGFzYWRoYXNiZCBhc2RhZHMgc2Rhc3AgZGFzIGRhc2RhcyBhc2RhcyBkYXNkIGFzZGFzZGFzZCBhcyBkYXNhZGFzIGFzIGRhcyBkYXNhZGFzIGFzIGRhcyBkYXNhZGFzZGFzZCBhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGFzIGRhcyBkYXNhIGRhcyBkYXNhZGFzIGRhcyBkYXNhZGphcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhZGFzIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhIGRhcyBkYXNhZGphcyIsImlhdCI6MTYzNDEwNTUyMn0.S7G4f8pW7sGJ7t9PIShNElA0RRve-HlPfZRvX8hnZ6c";
 
     o.TokenValidationParameters = new TokenValidationParameters
