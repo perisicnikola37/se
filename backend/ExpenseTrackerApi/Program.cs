b5baeef7b8cfc6c8163a6a6daed7ebdb22a617b2
Nikola <nikola.perisic@vegait.rs>
2024-01-22 09:12:48 +0100

    remove warning: Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'

--- backend/ExpenseTrackerApi/Program.cs
+++ backend/ExpenseTrackerApi/Program.cs
@@ -1,44 +1,42 @@
-using Microsoft.AspNetCore.RateLimiting;
-using System.Threading.RateLimiting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
+using System.Threading.RateLimiting;
+using Domain.Interfaces;
 using ExpenseTrackerApi.Middlewares;
 using Infrastructure.Contexts;
-using Domain.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var configuration = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+var configuration = builder.Configuration;
 // disabled
 // builder.Services.AddHttpLogging(o => { });
 builder.Services.AddCors(options =>
 {
-	options.AddDefaultPolicy(
-		policy =>
-		{
-			policy.WithOrigins("http://example.com");
-		});
+    options.AddDefaultPolicy(
+        policy => { policy.WithOrigins("http://example.com"); });
 });
 
 // add DB context
-string connectionString = configuration?["DefaultConnection"] ?? "DefaultConnection";
+var connectionString = configuration?["DefaultConnection"] ?? "DefaultConnection";
 
 builder.Services.AddDbContext<MainDatabaseContext>(options =>
 {
-	options.UseMySql(
-	  connectionString,
-		new MySqlServerVersion(new Version(8, 0, 35))
-	);
+    options.UseMySql(
+        connectionString,
+        new MySqlServerVersion(new Version(8, 0, 35))
+    );
 });
 
 builder.Services.AddAuthentication();
 
 // important for adding routes based on controllers
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
-	options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore
 );
 
 // services
@@ -46,29 +44,29 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<EmailService>();
 builder.Services.AddScoped<GetAuthenticatedUserIdService>();
 
-
 builder.Services.AddAuthentication(options =>
 {
-	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(o =>
 {
-	string validIssuer = configuration?["Jwt:Issuer"] ?? "https://joydipkanjilal.com/";
-	string validAudience = configuration?["Jwt:Audience"] ?? "https://joydipkanjilal.com/";
-	string issuerSigningKey = configuration?["Jwt:Key"] ?? "ddsadhasbd asdadsad sdas dasd asdasdasd as dasd sad sadas dadssndn asdnasjdnas jd asdas dasjdnas jn dsjan dasjn djasn djasndasjndjasndajsn djnasjnd";
+    var validIssuer = configuration?["Jwt:Issuer"] ?? "https://joydipkanjilal.com/";
+    var validAudience = configuration?["Jwt:Audience"] ?? "https://joydipkanjilal.com/";
+    var issuerSigningKey = configuration?["Jwt:Key"] ??
+                           "ddsadhasbd asdadsad sdas dasd asdasdasd as dasd sad sadas dadssndn asdnasjdnas jd asdas dasjdnas jn dsjan dasjn djasn djasndasjndjasndajsn djnasjnd";
 
-	o.TokenValidationParameters = new TokenValidationParameters
-	{
-		ValidIssuer = validIssuer,
-		ValidAudience = validAudience,
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = validIssuer,
+        ValidAudience = validAudience,
 
-		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey)),
-		ValidateIssuer = true,
-		ValidateAudience = true,
-		ValidateLifetime = false,
-		ValidateIssuerSigningKey = true
-	};
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
 });
 
 builder.Services.AddAuthorization();
@@ -77,13 +75,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddRateLimiter(rateLimiterOptions => rateLimiterOptions
-	.AddFixedWindowLimiter(policyName: "fixed", options =>
-	{
-		options.PermitLimit = 4;
-		options.Window = TimeSpan.FromSeconds(12);
-		options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-		options.QueueLimit = 2;
-	}));
+    .AddFixedWindowLimiter("fixed", options =>
+    {
+        options.PermitLimit = 4;
+        options.Window = TimeSpan.FromSeconds(12);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 2;
+    }));
 
 var app = builder.Build();
 
@@ -92,8 +90,8 @@ app.UseRateLimiter();
 
 // if (app.Environment.IsDevelopment())
 // {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 // }
 
 app.UseHttpsRedirection();
@@ -110,4 +108,3 @@ app.UseMiddleware<ClaimsMiddleware>();
 app.UseCors();
 
 app.Run();
-
