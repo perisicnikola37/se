5ffa1ccac2042070269ff11846a6bed290ca2066
Nikola <nikola.perisic@vegait.rs>
2024-01-23 15:23:56 +0100

    Modify BlogController.cs file

--- backend/Presentation/Controllers/BlogController.cs
+++ backend/Presentation/Controllers/BlogController.cs
@@ -7,27 +7,27 @@ namespace Presentation.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BlogController(BlogService blogService) : ControllerBase
+public class BlogController(BlogService _blogService) : ControllerBase
 {
 	// GET: api/Blog
 	[HttpGet]
 	public async Task<ActionResult<IEnumerable<BlogDTO>>> GetBlogs()
 	{
-		return Ok(await blogService.GetBlogsAsync());
+		return Ok(await _blogService.GetBlogsAsync());
 	}
 
 	// GET: api/Blog/5
 	[HttpGet("{id}")]
 	public async Task<ActionResult<SingleBlogDTO>> GetBlog(int id)
 	{
-		return await blogService.GetBlogAsync(id);
+		return await _blogService.GetBlogAsync(id);
 	}
 
 	// PUT: api/Blog/5
 	[HttpPut("{id}")]
 	public async Task<IActionResult> PutBlog(int id, Blog blog)
 	{
-		return await blogService.UpdateBlogAsync(id, blog, this);
+		return await _blogService.UpdateBlogAsync(id, blog, this);
 	}
 
 	// POST: api/Blog
@@ -35,13 +35,13 @@ public class BlogController(BlogService blogService) : ControllerBase
 	public async Task<ActionResult<Blog>> PostBlog(Blog blog)
 	{
 
-		return await blogService.CreateBlogAsync(blog, this);
+		return await _blogService.CreateBlogAsync(blog, this);
 	}
 
 	// DELETE: api/Blog/5
 	[HttpDelete("{id}")]
 	public async Task<IActionResult> DeleteBlog(int id)
 	{
-		return await blogService.DeleteBlogAsync(id);
+		return await _blogService.DeleteBlogAsync(id);
 	}
 }
