bbbf68462a180dc7070eedf34dc25bebb3a8e828
Nikola <nikola.perisic@vegait.rs>
2024-01-23 11:48:32 +0100

    refactor: AuthTests.cs file

--- backend/ExpenseTrackerApi.Tests/AuthTests.cs
+++ backend/ExpenseTrackerApi.Tests/AuthTests.cs
@@ -13,7 +13,11 @@ public class AuthControllerTests
 	[Fact]
 	public async Task LogInUser_ReturnsOkResult()
 	{
+		// Arrange
+		// Create a mock user object for login with email and password
 		var user = new LogInUser { Email = "nikola@e-invoices.online", Password = "06032004" };
+
+		// Create a mock user object with a token for the expected result
 		var userWithToken = new LoggedInUser
 		{
 			Username = "nikola",
@@ -21,26 +25,34 @@ public class AuthControllerTests
 			Token = "someToken"
 		};
 
+		// Set up the mock AuthService to return the mock user with a token when LogInUserAsync is called
 		authServiceMock.Setup(x => x.LogInUserAsync(user)).ReturnsAsync(userWithToken);
 
 		var authController = new AuthController(dbContextMock.Object, authServiceMock.Object, validatorMock.Object);
 
+		// Act
 		var result = await authController.LogInUser(user);
 
+		// Assert
 		Assert.IsType<ActionResult<User>>(result);
 	}
 
 	[Fact]
 	public async Task LogInUser_ReturnsNotFound()
 	{
+		// Arrange
+		// Create a mock user object for login with invalid email address
 		var user = new LogInUser { Email = "nonexistent@e-invoices.online", Password = "password123" };
 
+		// Set up the mock AuthService to return null when LogInUserAsync is called for a nonexistent user
 		authServiceMock.Setup(x => x.LogInUserAsync(user)).ReturnsAsync((LoggedInUser)null!);
 
 		var authController = new AuthController(dbContextMock.Object, authServiceMock.Object, validatorMock.Object);
 
+		// Act
 		var result = await authController.LogInUser(user);
 
+		// Assert
 		var actionResult = Assert.IsType<ActionResult<User>>(result);
 		var notFoundResult = Assert.IsType<NotFoundObjectResult>(actionResult.Result);
 
