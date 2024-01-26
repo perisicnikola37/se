9dbca107dc2f722cb7c26717b1b82bf9b0e22d3f
Nikola <nikola.perisic@vegait.rs>
2024-01-26 14:25:38 +0100

    Fix PaginationFilterDto.cs file

--- backend/Contracts/Filter/PaginationFilterDto.cs
+++ backend/Contracts/Filter/PaginationFilterDto.cs
@@ -1,7 +1,16 @@
 namespace Contracts.Filter;
-
-public class PaginationFilterDto(int pageNumber, int pageSize)
+public class PaginationFilterDto
 {
-    public int PageNumber { get; set; } = pageNumber < 1 ? 1 : pageNumber;
-    public int PageSize { get; set; } = pageSize > 10 ? 10 : pageSize;
+	public int PageNumber { get; set; }
+	public int PageSize { get; set; }
+	public PaginationFilterDto()
+	{
+		this.PageNumber = 1;
+		this.PageSize = 10;
+	}
+	public PaginationFilterDto(int pageNumber, int pageSize)
+	{
+		this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
+		this.PageSize = pageSize > 10 ? 10 : pageSize;
+	}
 }
