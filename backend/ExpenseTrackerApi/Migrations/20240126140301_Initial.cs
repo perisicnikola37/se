d12612757afca123917f01d9faa18a57a70b1a65
Nikola <nikola.perisic@vegait.rs>
2024-01-26 15:03:19 +0100

    Resolve and remove all warnings during building

--- backend/ExpenseTrackerApi/Migrations/20240126140301_Initial.cs
+++ backend/ExpenseTrackerApi/Migrations/20240126140301_Initial.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ExpenseTrackerApi.Migrations
 {
     /// <inheritdoc />
-    public partial class i : Migration
+    public partial class Initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -186,7 +186,7 @@ namespace ExpenseTrackerApi.Migrations
             migrationBuilder.InsertData(
                 table: "Users",
                 columns: new[] { "Id", "AccountType", "AccountTypeEnum", "CreatedAt", "Email", "Password", "Username" },
-                values: new object[] { 1, "Administrator", 1, new DateTime(2024, 1, 25, 9, 2, 54, 3, DateTimeKind.Local).AddTicks(3731), "admin@gmail.com", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "Administrator" });
+                values: new object[] { 1, "Administrator", 1, new DateTime(2024, 1, 26, 15, 3, 0, 492, DateTimeKind.Local).AddTicks(1634), "admin@gmail.com", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "Administrator" });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Blogs_UserId",
