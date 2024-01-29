00aef86beb565b245ef426f87894c4c2b2acb146
Nikola <nikola.perisic@vegait.rs>
2024-01-29 09:00:08 +0100

    fix: ExceptionMiddleware.cs file

--- backend/ExpenseTrackerApi/Middlewares/ExceptionMiddleware.cs
+++ backend/ExpenseTrackerApi/Middlewares/ExceptionMiddleware.cs
@@ -1,81 +1,96 @@
+using System;
 using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Contracts.Dto;
 using Domain.Exceptions;
-using Newtonsoft.Json;
 
-namespace ExpenseTrackerApi.Middlewares;
-
-public class GlobalExceptionHandlerMiddleware(RequestDelegate next)
+namespace ExpenseTrackerApi.Middlewares
 {
-	public async Task InvokeAsync(ILogger<GlobalExceptionHandlerMiddleware> logger, HttpContext context)
+	public class GlobalExceptionHandlerMiddleware
 	{
-		try
-		{
-			await next(context);
-		}
-		catch (Exception ex)
-		{
-			logger.LogError(ex, ex.Message);
+		private readonly RequestDelegate _next;
+		private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
 
-			await HandleExceptionAsync(context, ex);
+		public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+		{
+			_next = next ?? throw new ArgumentNullException(nameof(next));
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, ex.Message);
+				await HandleExceptionAsync(context, ex);
+			}
+		}
+
+		private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+		{
+			var code = HttpStatusCode.InternalServerError;
+			string? fileName = null;
+
+			switch (exception)
+			{
+				case InvalidAccountTypeException invalidAccountTypeException:
+					code = HttpStatusCode.BadRequest;
+					fileName = invalidAccountTypeException.FileName;
+					break;
+
+				case ConflictException conflictException:
+					code = HttpStatusCode.Conflict;
+					fileName = conflictException.FileName;
+					break;
+
+				case DatabaseException databaseException:
+					code = HttpStatusCode.InternalServerError;
+					fileName = databaseException.FileName;
+					break;
+
+				case OnModelCreatingException onModelCreatingException:
+					code = HttpStatusCode.InternalServerError;
+					fileName = onModelCreatingException.FileName;
+					break;
+				case UnauthorizedException unauthorizedException:
+					code = HttpStatusCode.Unauthorized;
+					fileName = unauthorizedException.FileName;
+					break;
+
+				default:
+					break;
+			}
+
+
+			var errorResponse = new ErrorResponse
+			{
+				Error = exception.Message,
+				StatusCode = (int)code,
+				Path = context.Request.Path,
+				FileName = fileName
+			};
+
+			var result = JsonConvert.SerializeObject(errorResponse);
+			context.Response.ContentType = "application/json";
+			context.Response.StatusCode = (int)code;
+
+			return context.Response.WriteAsync(result);
 		}
 	}
 
-	private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+	public static class GlobalExceptionHandlerMiddlewareExtensions
 	{
-		var code = HttpStatusCode.InternalServerError;
-		string? fileName = null;
-
-		switch (exception)
+		public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder)
 		{
-			case InvalidAccountTypeException invalidAccountTypeException:
-				code = HttpStatusCode.BadRequest;
-				fileName = invalidAccountTypeException.FileName;
-				break;
-
-			case ConflictException conflictException:
-				code = HttpStatusCode.Conflict;
-				fileName = conflictException.FileName;
-				break;
-
-			case DatabaseException databaseException:
-				code = HttpStatusCode.InternalServerError;
-				fileName = databaseException.FileName;
-				break;
-
-			case OnModelCreatingException onModelCreatingException:
-				code = HttpStatusCode.InternalServerError;
-				fileName = onModelCreatingException.FileName;
-				break;
-			case UnauthorizedException unauthorizedException:
-				code = HttpStatusCode.Unauthorized;
-				fileName = unauthorizedException.FileName;
-				break;
-				
-			default:
-				break;
+			return builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 		}
-
-		var errorResponse = new ErrorResponse
-		{
-			Error = exception.Message,
-			StatusCode = (int)code,
-			Path = context.Request.Path,
-			FileName = fileName
-		};
-
-		var result = JsonConvert.SerializeObject(errorResponse);
-		context.Response.ContentType = "application/json";
-		context.Response.StatusCode = (int)code;
-
-		return context.Response.WriteAsync(result);
-	}
-}
-
-public static class GlobalExceptionHandlerMiddlewareExtensions
-{
-	public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder)
-	{
-		return builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 	}
 }
