72c4ea0ef52ab1882530330c47340537b02e3879
Nikola <nikola.perisic@vegait.rs>
2024-02-05 16:10:20 +0100

    Finish Profile.tsx file

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import useCurrentUser from '../hooks/Authentication/GetUserHook';
 import useDeleteAccount from '../hooks/Authentication/DeleteAccountHook';
 import BorderLinearProgress from '../components/Progress';
+import { Button } from '@mui/material';
 
 const Profile = () => {
     const { loadCurrentUser, user } = useCurrentUser()
@@ -34,15 +35,15 @@ const Profile = () => {
                     <div className="grid grid-cols-1 md:grid-cols-3">
                         <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
                             <div>
-                                <p className="font-bold text-gray-700 text-xl">{user?.expenses.length}</p>
+                                <p className="font-bold text-red-500 text-xl">{user?.expenses.length}</p>
                                 <p className="text-gray-400">Expenses</p>
                             </div>
                             <div>
-                                <p className="font-bold text-gray-700 text-xl">{user?.incomes.length}</p>
+                                <p className="font-bold text-green-500 text-xl">{user?.incomes.length}</p>
                                 <p className="text-gray-400">Incomes</p>
                             </div>
                             <div>
-                                <p className="font-bold text-gray-700 text-xl">${totalIncome}</p>
+                                <p className="font-bold text-[#2563EB] text-xl">${totalIncome}</p>
                                 <p className="text-gray-400">Total revenue</p>
                             </div>
                         </div>
@@ -58,9 +59,9 @@ const Profile = () => {
                         </div>
 
                         <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
-                            <button onClick={handleDeleteAccount} className="text-white py-2 px-4 uppercase rounded bg-red-500 hover:bg-red-600 shadow hover:shadow-lg font-medium transition transform">
+                            <Button onClick={handleDeleteAccount} variant="outlined" color="error">
                                 Delete account
-                            </button>
+                            </Button>
                         </div>
                     </div>
 
