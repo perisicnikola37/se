d18808c78ffcdf6231d3a46a5150dde379c0afc2
Nikola <nikola.perisic@vegait.rs>
2024-02-09 10:28:09 +0100

    Create appsettings.Docker.json file

--- backend/ExpenseTrackerApi/conf/DatabaseConfig.cs
+++ backend/ExpenseTrackerApi/conf/DatabaseConfig.cs
@@ -2,39 +2,37 @@ namespace ExpenseTrackerApi.conf;
 
 using Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
 public static class DatabaseConfig
 {
+	// To use Docker, set useDocker environment variable to true in appsettings.json
 	public static void ConfigureDatabaseContext(this WebApplicationBuilder builder, IConfiguration configuration)
 	{
 		bool useDocker = configuration["useDocker"] != null && bool.TryParse(configuration["useDocker"], out var parsedValue) && parsedValue;
 
+		var configBuilder = new ConfigurationBuilder()
+			.SetBasePath(AppContext.BaseDirectory)
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
 		if (useDocker)
 		{
-			builder.Services.AddDbContext<DatabaseContext>(options =>
-			{
-				options.UseMySql(
-					"server=mysql;user=root;port=3306;Connect Timeout=5;database=first_database;password=password",
-					new MySqlServerVersion(new Version(8, 0, 35)),
-					b => b.MigrationsAssembly("ExpenseTrackerApi")
-				);
-			});
+			configBuilder.AddJsonFile("appsettings.Docker.json", optional: false, reloadOnChange: true);
 		}
-		else
-		{
-			var connectionString = configuration["DefaultConnection"];
-			if (connectionString == null) throw new ArgumentNullException(nameof(connectionString), "DefaultConnection is null in environment variables. Please configure it.");
 
-			builder.Services.AddDbContext<DatabaseContext>(options =>
-			{
-				options.UseMySql(
-					connectionString,
-					new MySqlServerVersion(new Version(8, 0, 35)),
-					b => b.MigrationsAssembly("ExpenseTrackerApi")
-				);
-			});
-		}
+		var connectionString = configBuilder.Build()["DefaultConnection"];
+		if (connectionString == null) throw new ArgumentNullException(nameof(connectionString), "DefaultConnection is null in environment variables. Please configure it.");
+
+		builder.Services.AddDbContext<DatabaseContext>(options =>
+		{
+			options.UseMySql(
+				connectionString,
+				new MySqlServerVersion(new Version(8, 0, 35)),
+				b => b.MigrationsAssembly("ExpenseTrackerApi")
+			);
+		});
 	}
 }
